3c625bc9fe1f66497383e4cb72427a01b2004ae0
Melissa Ribeiro <86805880+MelRibeiro@users.noreply.github.com>
2021-11-02 16:30:27 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function read() {
+    window.open('https://www.amazon.com.br/Assim-que-Acaba-Colleen-Hoover/dp/8501112518/ref=asc_df_8501112518/?tag=googleshopp00-20&linkCode=df0&hvadid=379751484847&hvpos=&hvnetw=g&hvrand=7696827557803379249&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9047743&hvtargid=pla-810156783974&psc=1')
+}
+
+function moreInf() {
+    window.open('https://www.skoob.com.br/livro/resenhas/738219/mais-gostaram')
+}
